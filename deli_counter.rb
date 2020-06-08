7b920b938da24df1c7d826609bb32dfa462e9565
Olivia Cubela <oliviacubela@gmail.com>
2020-06-08 18:31:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli = [ ]
+  if katz_deli != 0
+    puts "The line is currently empty."
+  else
+    puts "Welcome to Katz Deli!"
+  end
+end
+
+
+#build the line method that shows everyone their current place in the line. If there is nobody in line, it should say "The line is currently empty."
+
+
+#def now_serving
+
+#end
